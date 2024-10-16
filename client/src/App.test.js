f890c864bca3a1fcb9b9c193749dbb6c94597ace
Chiemerie <alexemerie7@gmail.com>
2024-10-16 06:26:32 +0100

    Removed tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,1 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Fibonacci/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders learn react link', () => {});
