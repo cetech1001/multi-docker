08c300d017690e06d03472ec479f36a80e594880
Chiemerie <alexemerie7@gmail.com>
2024-10-16 06:19:40 +0100

    moved browser router wrap to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
 import './App.css';
 import OtherPage from "./OtherPage";
 import Fib from "./Fib";
-import {Link, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Fibonacci Calculator</p>
-        <Link className="App-link" to="/">Home</Link>
-        <Link className="App-link" to="/otherpage">Other Page</Link>
-      </header>
-        <Routes>
-            <Route path="/" element={<Fib />} />
-            <Route path="/otherpage" element={<OtherPage />} />
-        </Routes>
-    </div>
+      <BrowserRouter>
+          <div className="App">
+              <header className="App-header">
+                  <p>Fibonacci Calculator</p>
+                  <Link className="App-link" to="/">Home</Link>
+                  <Link className="App-link" to="/otherpage">Other Page</Link>
+              </header>
+              <Routes>
+                  <Route path="/" element={<Fib/>}/>
+                  <Route path="/otherpage" element={<OtherPage/>}/>
+              </Routes>
+          </div>
+      </BrowserRouter>
   );
 }
 
